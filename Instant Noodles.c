a61ef1559dd2115458120e074bb0f0129488880d
Arunkasi072 <24b11ec072@adityauniversity.in>
2024-11-21 07:15:02 +0530

    Uploading code: Instant Noodles.c

---
+++ Instant Noodles.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, maxCustomers;
+
+    
+    scanf("%d %d", &X, &Y);
+
+   
+    maxCustomers = X * Y;
+
+    
+    printf("%d\n", maxCustomers);
+
+    return 0;
+}
