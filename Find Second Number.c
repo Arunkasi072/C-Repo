7d4a67c08803917b7d14cdade6b75a18ec3eebba
Arunkasi072 <24b11ec072@adityauniversity.in>
2024-11-21 07:17:06 +0530

    Uploading code: Find Second Number.c

---
+++ Find Second Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, secondNumber;
+
+
+    scanf("%d %d", &X, &Y);
+
+    
+    secondNumber = 2 * X - Y;
+
+    printf("%d\n", secondNumber);
+
+    return 0;
+}
