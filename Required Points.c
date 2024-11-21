5311efe2d9bd71a237dd320dd5a74ef28c541134
Arunkasi072 <24b11ec072@adityauniversity.in>
2024-11-21 06:48:51 +0530

    Uploading code: Required Points.c

---
+++ Required Points.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, specialAttacks;
+
+    
+    scanf("%d %d", &A, &B);
+
+   
+    specialAttacks = B / A;
+
+  
+    printf("%d\n", specialAttacks);
+
+    return 0;
+}
